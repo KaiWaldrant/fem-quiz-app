56d67d22db81f8895fb747b784fd092e55b8ee20
Kai Waldrant <kai.waldrant@outlook.com>
2025-12-10 23:08:31 +0100

    WIP theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 // import { useState } from "react";
-import { ThemeSwitcher } from "./components/theme-button";
 
 function App() {
   // const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <ThemeSwitcher />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
