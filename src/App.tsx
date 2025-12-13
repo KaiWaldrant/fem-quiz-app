4a9d89a7e21e2a62273e09237c00cdaad052b644
Kai Waldrant <kai.waldrant@outlook.com>
2025-12-13 21:30:31 +0100

    WIP add chadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
     // const [count, setCount] = useState(0)
 
     return (
         <>
+            <div className="p-8">
+                <Button>It works!</Button>
+                <Button variant="purple">Outline</Button>
+            </div>
             {/*<div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
