139c44bffb62c68254d1bb3939e5ffcd5dd38d97
Kai Waldrant <kai.waldrant@outlook.com>
2025-12-15 23:41:52 +0100

    style home screen and add text design css

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 
 export function Layout() {
     return (
-        <div className="flex flex-col min-h-svh min-w-svw bg-grey-50 dark:bg-blue-850">
+        <div className="flex flex-col min-h-svh min-w-svw bg-grey-50 dark:bg-blue-850 text-blue-90 dark:text-white0">
             <Header />
-            <main>
+            <main className="grid gap-10 px-6 py-8 ">
                 <Outlet />
             </main>
         </div>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,22 +3,23 @@ import { Button } from "@/components/ui/button";
 export function Home() {
     return (
         <>
-            <main>
-                <section>
-                    <h1 className="heading-1">
-                        Welcome to the <span>Frontend Quiz!</span>
-                    </h1>
-                    <h2 className="subheading">
-                        Pick a subject to get started.
-                    </h2>
-                </section>
-                <section className="catagories">
-                    <Button>HTML</Button>
-                    <Button>CSS</Button>
-                    <Button>Javascript</Button>
-                    <Button>Accessibility</Button>
-                </section>
-            </main>
+            <section className="grid gap-4">
+                <h1 className="text-blue-900 text-preset-2-light-mobile">
+                    Welcome to the{" "}
+                    <span className="text-preset-2-medium-mobile">
+                        Frontend Quiz!
+                    </span>
+                </h1>
+                <h2 className="text-preset-5-mobile italic text-grey-500">
+                    Pick a subject to get started.
+                </h2>
+            </section>
+            <section className="grid gap-4">
+                <Button className="p-4 text-preset-4-mobile">HTML</Button>
+                <Button>CSS</Button>
+                <Button>Javascript</Button>
+                <Button>Accessibility</Button>
+            </section>
         </>
     );
 }
