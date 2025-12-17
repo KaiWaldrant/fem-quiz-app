a7c5a32d32e9d937bc589e8c87bd878b8970769c
Kai Waldrant <kai.waldrant@outlook.com>
2025-12-17 23:27:06 +0100

    simplify background image

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,11 +1,9 @@
 import { Header } from "@/components/header";
 import { Outlet } from "react-router-dom";
-import { BackgroundSVG } from "@/components/background-SVG";
 
 export function Layout() {
     return (
-        <div className="flex flex-col min-h-svh min-w-svw text-blue-900 dark:text-white">
-            <BackgroundSVG />
+        <div className="flex flex-col min-h-svh min-w-svw text-blue-900 dark:text-white bg-svg bg-grey-50 dark:bg-blue-900">
             <Header />
             <main className="grid gap-10 px-6 py-8 ">
                 <Outlet />
